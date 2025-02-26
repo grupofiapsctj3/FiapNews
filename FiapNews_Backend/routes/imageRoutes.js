a6b190b0170f99bc9bc3c1e3985b54e85fb30910
gmoratelli <38704443+gmoratelli@users.noreply.github.com>
2025-02-26 20:06:52 -0300

    ultimas alterações

--- FiapNews_Backend/routes/imageRoutes.js
+++ FiapNews_Backend/routes/imageRoutes.js
@@ -4,17 +4,13 @@ import { uploadImage, saveImageMetadata, getImages } from "../controllers/imageC
 
 const router = express.Router();
 
-// 🔥 Configuração do Multer para receber a imagem
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-// 🔥 Rota POST para upload de imagens (Filestack)
+
 router.post("/upload", upload.single("file"), uploadImage);
-
-// 🔥 Rota POST para salvar metadados (URL, legenda e descrição)
 router.post("/save", saveImageMetadata);
-
-// 🔥 Rota GET para listar imagens do banco ordenadas por data
 router.get("/all", getImages);
 
 
